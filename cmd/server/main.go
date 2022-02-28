6d3c34e14833dee1e5e352dff9ea061f0925c953
Joost van der Griendt <joostvdg@gmail.com>
2022-02-28 23:29:50 +0100

    finish chapter10 and add ignore files

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"github.com/joostvdg/proglog/internal/server"
+	"context"
+	"flag"
+	"fmt"
 	"log"
+
+	"google.golang.org/grpc"
+
+	api "github.com/joostvdg/proglog/api/v1"
 )
 
 func main() {
-
-	srv := server.NewHTTPServer(":8081")
-	log.Fatal(srv.ListenAndServe())
+	addr := flag.String("addr", "127.0.0.1:8400", "service address")
+	flag.Parse()
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := api.NewLogClient(conn)
+	ctx := context.Background()
+	res, err := client.GetServers(ctx, &api.GetServerRequest{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("servers:")
+	for _, server := range res.Servers {
+		fmt.Printf("\t- %v\n", server)
+	}
 }
